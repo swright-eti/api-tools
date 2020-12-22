800a0e3f517e49e5d4308c3c0bfc43c432303575
etisteve <swright@etisoftware.com>
2020-12-22 11:08:56 -0500

    Added kubefwd functionality

--- api-tester/api-test.py
+++ api-tester/api-test.py
@@ -2,6 +2,7 @@ import json
 import requests
 import argparse
 import getpass
+import re
 #
 # Setup the parser
 #
@@ -16,23 +17,10 @@ args = parser.parse_args()
 # Setup the program arguments
 # 
 protocol = "http"
-base_url = protocol + "://" + args.base_url
+base_url = args.base_url
 username = args.username
 org = args.org_label
-endpoints = []
 
-if(args.endpoints):
-    endpoints = args.endpoints
-else:
-    endpoints = [
-    "/device-api/v1/devices",
-    "/location-api/v1/locations",
-    "/ont-api/v1/onts",
-    "/phone-api/v1/phones",
-    "/service-api/v1/services",
-    "/subscriber-api/v1/subscribers",
-    "/work-queue-api/v1/controller-work"
-    ]
 #
 # Do the special to get the password prompt
 #
@@ -41,15 +29,15 @@ password = getpass.getpass(prompt='Password: ', stream=None)
 #
 # Will fetch and return a token from the auth-api at the base_url
 #  
-def get_token():
+def get_token(endpoint):
     headers = {'Content-Type': 'application/json', 'accept': 'application/json'}
-    api_url = base_url + "/auth-api/auth"
-    print("Getting token from " + api_url)
+    print("Getting token from " + endpoint)
     args={"username":username,"password":password,"org":org}
-    response = requests.post(api_url, headers=headers, json=args)
+    response = requests.post(endpoint, headers=headers, json=args)
 
     if response.status_code == 200:
         token_obj =  json.loads(response.content.decode('utf-8'))
+        print("Got a token: " + token_obj['jwt'])
         return token_obj['jwt']
     else:
         print(response)
@@ -59,9 +47,8 @@ def get_token():
 # Call the given REST endpoint with the provided token and return an object count
 #
 def call_rest_endpoint(endpoint, token):
-    api_url = base_url + endpoint
     headers = {'Authorization': 'Bearer ' + token, 'accept': 'application/hal+json'}
-    response = requests.get(api_url, headers=headers)
+    response = requests.get(endpoint, headers=headers)
 
     if response.status_code == 200:
         response_obj =  json.loads(response.content.decode('utf-8'))
@@ -69,18 +56,75 @@ def call_rest_endpoint(endpoint, token):
     else:
         print("Resonse code: " + str(response.status_code) + " for " + endpoint)
         return None
+#
+# If using kubefwd then this function will extract all the service names from the /etc/hosts file
+#
+def get_services_from_etc_hosts():
+    services = []
+    with open('/etc/hosts') as hosts:
+        for line in hosts.readlines():
+            res = re.search(r'^[0-9.]+\W+([a-z]+-api-service)', line) 
+            if res:
+                services.append(res.group(1))
+    return services
 
+#
+# Build a dict of REST urls to K8s services names
+def build_endpoint_dict():
+    endpoints = {}
+    if base_url == 'kubefwd':
+        services = get_services_from_etc_hosts()
+        for service in services:
+            res = re.search(r'^(\w+)-(\w+)-service', service)
+            if res:
+                api_name = res.group(1) + '-' + res.group(2)
+                if api_name == 'auth-api':
+                    endpoint = protocol + '://' + service + "/" + api_name + "/" + res.group(1)
+                else:
+                    endpoint = protocol + '://' + service + "/" + api_name + "/v1/" + res.group(1) + "s"
+                endpoints[api_name] = endpoint
+    else:
+        if(args.endpoints):
+            for endpoint in args.endpoints:
+                res = re.search(r'^\/(\w+-\w+)', endpoint)
+                if res:
+                    endpoints[res.group(1)] = protocol + '://' + base_url + endpoint
+        else:
+            endpoints = {
+            "device-api" : protocol + '://' + base_url + "/device-api/v1/devices",
+            "location-api" : protocol + '://' + base_url + "/location-api/v1/locations",
+            "ont-api" : protocol + '://' + base_url + "/ont-api/v1/onts",
+            "phone-api" : protocol + '://' + base_url + "/phone-api/v1/phones",
+            "service-api" : protocol + '://' + base_url + "/service-api/v1/services",
+            "subscriber-api" : protocol + '://' + base_url + "/subscriber-api/v1/subscribers",
+            "controller-work-api" : protocol + '://' + base_url + "/work-queue-api/v1/controller-work"
+            }
+        endpoints["auth-api"] = protocol + '://' + base_url + "/auth-api/auth"
 
-token = get_token()
-print("Got a token: " + token)
+    return endpoints
 
+#
+# The program
+#
+endpoints = build_endpoint_dict()
 
-if(token is None):
-    print("No token found")
+if len(endpoints) > 0 :
+    token = get_token(endpoints['auth-api'])
+
+    if(token is None):
+        print("No token found, cannot continue")
+    else:
+        for api in endpoints:
+            if api != "auth-api":
+                call_rest_endpoint(endpoints[api], token)
 else:
-    if(len(endpoints) > 0):
-        for endpoint in endpoints:
-            call_rest_endpoint(endpoint, token)
+    print("No endpoints specified, nothing to do")
+
+
+
+
+
+
 
 
 
